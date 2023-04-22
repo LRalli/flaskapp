29b7f2971d721af05ca37073eb700cfbbfcefbd8
LRalli <lorenzoralli999@gmail.com>
2023-04-22 17:33:28 +0200

    form tes 10

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, render_template_string, send_file, flash, redirect, url_for
+from flask import Flask, render_template, request, render_template_string
 import os
 
 HTML = '<html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><title>Malware Martini</title><link rel="stylesheet" href="/static/assets/css/member.css" /><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"></head><body><nav class="navbar navbar-expand-lg navbar-dark bg-dark"><div class="container-fluid"><a class="navbar-brand" href="/">Malware Martini</a><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarSupportedContent"><ul class="navbar-nav me-auto mb-2 mb-lg-0"><li class="nav-item"><a class="nav-link" href="/team">Executive Team</a></li><li class="nav-item"><a class="nav-link" href="/contacts">Contacts</a></li></ul></div></div></nav><br><p id="error">{} not found</p><footer id="footererror"class="text-center text-white"style="background-color: #343a40"><br /><p>© 2023 Copyright: <a class="text-white" href="#">Malware Martini</a></p></footer></body></html>'
@@ -51,6 +51,9 @@ def confirm_message_file():
     with open('/var/www/html/flaskapp/message.txt', 'w') as file:
         file.write(message)
     return render_template('index.html')
-	
 
-		
+@app.route('/sent', methods=['GET'])
+def confirm_send():
+	with open('/var/www/html/flaskapp/message.txt', 'r') as file:
+		message = file.read()
+	return render_template('confirm_send.html', message=message)
