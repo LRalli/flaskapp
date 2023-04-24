af70b86e09efd55e8aab25549ffe7e9bf71d4c6e
LRalli <lorenzoralli999@gmail.com>
2023-04-24 12:22:53 +0200

    test msg

--- flaskapp.py
+++ flaskapp.py
@@ -50,8 +50,8 @@ def create_message_file():
 def confirm_message_file():
     name = request.form.get('name')
     message = request.form.get('message')
-    with open('/var/www/html/flaskapp/message.txt', 'w') as file:
-        file.write(name + ":" + message + "\r\n")
+    with open('/var/www/html/flaskapp/message.txt', 'a') as file:
+        file.write(name + ":" + message + "\n")
     return redirect("/sent")
 
 @app.route('/sent', methods=['GET'])
