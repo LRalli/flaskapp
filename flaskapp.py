fbce9ef7501fbedb2900fa0bde5e8ee4fcdc8093
LRalli <lorenzoralli999@gmail.com>
2023-04-24 12:18:10 +0200

    change msg text

--- flaskapp.py
+++ flaskapp.py
@@ -51,7 +51,7 @@ def confirm_message_file():
     name = request.form.get('name')
     message = request.form.get('message')
     with open('/var/www/html/flaskapp/message.txt', 'w') as file:
-        file.write(name + ":" + message)
+        file.write(name + ":" + message + "\n")
     return redirect("/sent")
 
 @app.route('/sent', methods=['GET'])
