887630307cbc45717e07afd7a8bd08dfc196137f
LRalli <lorenzoralli999@gmail.com>
2023-04-24 18:41:11 +0200

    test

--- flaskapp.py
+++ flaskapp.py
@@ -52,22 +52,16 @@ def confirm_message_file():
     name = request.form.get('name')
     message = request.form.get('message')
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    path = "/var/www/html/flaskapp/"
+    path = "C:\\"
     filename = name
     with open(os.path.join(path, filename + ".txt"), 'a+') as file:
         file.write(name + " : " + message + '\t' + " [ " + timestamp + " ] " + '\n')
-    return redirect(url_for('sent', file = filename, path = path))
+    return redirect('/sent?file=' + filename)
 
 @app.route('/sent', methods=['GET'])
 def confirm_send():
-	filename = request.args.get('file')
-	path = request.args.get('path')
-	if not path or not filename:
-		abort(400, 'Missing parameters')
-	try:
-		file_path = os.path.join(path, filename + ".txt")
-		with open(file_path, 'r') as file:
-			message = file.read()
-	except FileNotFoundError:
-		abort(404, 'file not found')
-	return render_template('confirm_send.html', message = message)
+	filename = request.values.get('file')
+	path = "/"
+	with open(os.path.join(path, filename), 'r') as file:
+		message = file.read()
+	return render_template('confirm_send.html', message=message)
